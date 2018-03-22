af0595ef1f23602a1e1da2b480de29d112d64b4b
Edwin Alvarado <edwinalvaradoster@gmail.com>
2018-03-22 11:41:25 -0400

    Initial Commit

---
+++ EddystoneConnectionSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EddystoneConnectionSample
+//
+//  Created by Edwin Alvarado on 3/22/18.
+//  Copyright © 2018 Edwin Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
